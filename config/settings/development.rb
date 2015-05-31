94b4933c82c45a863096249ad40d5763c530d7df
Srikanth Gurram <2gurram@gmail.com>
2015-05-31 14:55:40 +0530

    Environment specific configurations added for Google Auth

--- config/settings/development.rb
+++ config/settings/development.rb
@@ -1,7 +1,5 @@
 SimpleConfig.for :application do
-  # group :auth do
-  #   set :client_id, 'CLIENT ID'
-  #   set :client_secret, 'CLIENT SECRET'
-  #   set :secret_token, 'SECRET TOKEN'
-  # end
+  set :client_id, '1086079076926-2375al5iftg91k5m0o1n8gls7cpicia1.apps.googleusercontent.com'
+  set :client_secret, 'k80aARHN7b1_Taf6_jZqseLn'
+  set :secret_token, '153bf9ba4f5bea7aaf3e58a58d3d37320ddea91a4ac66e779f0ea05f784104f46fcef89e0352fb583c84d39504d3653b7a1a8647beb8554b8500fea7e0c9a1ba'
 end
--- config/settings/production.rb
+++ config/settings/production.rb
@@ -1,7 +1,7 @@
 SimpleConfig.for :application do
-  # group :auth do
-  #   set :client_id, ENV['OAUTH2_CLIENT_ID']
-  #   set :client_secret, ENV['OAUTH2_CLIENT_SECRET']
-  #   set :secret_token, ENV['SECRET_TOKEN']
-  # end 
+  group :auth do
+    set :client_id, '1086079076926-f5c3hp8k9rosv16ut8efs3bdlb9jgpb5.apps.googleusercontent.com'
+    set :client_secret, 'Gv1vufzIArwZZaPAcKe2jCp2'
+    set :secret_token, '6a25cb14578f44b65c7f683dc50cfe2c3d7725ebc817a7cffdd5d93a5b585aa31cd82f5399c125d3284205078707d7cd2e33aa953f58e0dae868d3df644e6a85'
+  end 
 end
